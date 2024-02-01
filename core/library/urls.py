4fdb1592db1989318444373f5d8f7fa27e4153eb
sabarish1512 <1234567890@Sabarish>
2024-02-01 12:42:02 +0530

    final commit

---
+++ core/library/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import allbooks,search,addbook,deletebook,issuerequest,myissues,issue_book,return_book,requestedissues,myfines,allfines,deletefine,sort
+urlpatterns = [
+    path('',allbooks,name='home'),
+    path('search/',search),
+    path('sort/',sort),
+    path('addbook/',addbook),
+    path('deletebook/<int:bookID>/',deletebook),
+    path('request-book-issue/<int:bookID>/',issuerequest),
+    path('my-issues/',myissues),
+    path('my-fines/',myfines),
+    path('all-issues/',requestedissues),
+    path('all-fines/',allfines),
+    path('issuebook/<int:issueID>/',issue_book),
+    path('returnbook/<int:issueID>/',return_book),
+    path('delete-fine/<int:fineID>/',deletefine),
+]
